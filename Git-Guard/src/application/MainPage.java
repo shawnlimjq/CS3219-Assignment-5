80f151e7c8591921d7367642f4b822ff05b7f06f
shawnlimjq <shawnlimjq@hotmail.com>
2016-10-20 21:02:49 +0800

    Added in function to read enter key for the gitrepo link

--- Git-Guard/src/application/MainPage.java
+++ Git-Guard/src/application/MainPage.java
@@ -3,10 +3,13 @@ package application;
 import java.io.IOException;
 
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
 
@@ -43,6 +46,14 @@ public class MainPage extends AnchorPane {
 	
 	public void initialise() {
 		Platform.runLater( () -> this.requestFocus() );
+		githubRepoInput.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			public void handle(KeyEvent ke) {
+				if (ke.getCode().equals(KeyCode.ENTER)) {
+					//commands to show json stuff here
+				}
+			}
+		});
 	}
 	
 }
