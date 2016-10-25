e5b6192ee40100db0aa65b70f51d7c96620efdfd
wenhaohaoo <lwen.hao93@gmail.com>
2016-10-26 00:29:41 +0800

    Implemented methods for S3Connector

--- Git-Guard/src/application/S3Connector.java
+++ Git-Guard/src/application/S3Connector.java
@@ -1,38 +1,83 @@
 package application;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 
 public class S3Connector {
 
-	private static final String accessKey = "";
-	private static final String secretAccessKey = "";
-	private static final String bucketName = "";
-	private static final String folderName = "";
-	private static final String fileName = "";
+	private static final String SUFFIX = "/";
+	private static final String accessKey = "AKIAJICGFKMYAER4HNEQ";
+	private static final String secretAccessKey = "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";
+	private String bucketName;
+	private String folderName;
 	private AWSCredentials credentials;
 	private AmazonS3 s3client;
 	
-	public S3Connector() {
-		init();
+	public S3Connector(String bucketName, String folderName) {
+		init(bucketName, folderName);
 	}
 	
-	private void init() {
-
+	private void init(String bucketName, String folderName) {
+		this.bucketName = bucketName;
+		this.folderName = folderName;
+		credentials = new BasicAWSCredentials(accessKey, secretAccessKey);
+		s3client = new AmazonS3Client(credentials);
 	}
 	
-	public void downloadFile() {
-		
+	public void downloadFile(String fileName) {
+		try {
+			String pathName = folderName + SUFFIX + fileName;
+			
+			if (s3client.doesObjectExist(bucketName, pathName)) {
+				S3Object object = s3client.getObject(new GetObjectRequest(bucketName, pathName));
+			    InputStream stream = object.getObjectContent();
+			    BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(fileName));
+			    
+			    int bytesRead;
+			    byte[] content = new byte[1024];
+			    while ((bytesRead = stream.read(content)) != -1) {
+			      outputStream.write(content, 0, bytesRead);
+			    }
+			    
+			    // close resource even during exception
+			    stream.close();
+			    outputStream.close();
+			    
+			} else {
+				System.out.println("No existing data, creating new data file..");
+			}
+		    
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  catch (AmazonS3Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void uploadFile() {
-		
+	public void uploadFile(String fileName) {
+		String pathName = folderName + SUFFIX + fileName;
+		s3client.putObject(new PutObjectRequest(bucketName, pathName, 
+				new File(fileName))
+				.withCannedAcl(CannedAccessControlList.PublicRead));
 	}
 	
-	
-	
-	
+	public static void main(String[] args) {
+		S3Connector s3 = new S3Connector("cs3219.ass5", "ass5_data");
+		s3.downloadFile("data.properties");
+	}
 	
 }
